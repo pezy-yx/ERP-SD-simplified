751685491f464b6dedf44d08ff02d6d6a48b852e
Leopold122042 <160714082+Leopold122042@users.noreply.github.com>
2025-07-04 23:27:42 +0800

    feat:客户管理功能更新 (#5)
    导航栏，分区栏.vue文件更新，Assets贴图加入，目录更新，views加入BP管理视图
    
    Co-authored-by: Peyton <153476723+pezy-yx@users.noreply.github.com>

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -1,17 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
+// src/router/index.ts
+import { createRouter, createWebHistory } from "vue-router";
+import MaintainBusinessPartnerView from '@/views/MaintainBusinessPartnerView.vue'; 
+import TestPageErp from "@/test/varbox/TestPageErp.vue";
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import Home from '@/views/Home.vue';
 
 const routes = [
-    { path: '/', component: Home },
+  {
+    path: '/',
+    name: 'Home',
+    component: MaintainBusinessPartnerView // 你的系统主页或默认页面
+  },
+  {
+    path: '/maintain-bp',
+    name: 'MaintainBusinessPartner',
+    component: MaintainBusinessPartnerView // 实际的业务伙伴维护页面
+  },
+  {
+    path: '/test/erppage', // 定义一个专门用于测试的路由路径
+    name: 'MaintainBPTest',
+    component: TestPageErp // 引入你的测试文件
+  }，
     { path: '/login', component: Login },
     { path: '/register', component: Register }
+  // 你可以在这里添加其他页面路由
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(), // 使用 HTML5 History 模式
+  routes
 });
 
 export default router;
