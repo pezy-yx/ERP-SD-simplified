5a69a6c771fb2294d62ba888991c09fbc2611731
Peyton <153476723+pezy-yx@users.noreply.github.com>
2025-07-06 15:25:52 +0800

    完善登录和注册功能，实现部分输入控制，以及实现全局css变量控制 (#7)

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -5,12 +5,17 @@ import TestPageErp from "@/test/varbox/TestPageErp.vue";
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import Home from '@/views/Home.vue';
+import getMyPassBack from '@/views/GetMyPassBack.vue'; // 引入主页组件
 
 const routes = [
   {
+    path:'/',
+    redirect: '/login' // 默认重定向到登录页面
+  },
+  {
     path: '/',
     name: 'Home',
-    component: MaintainBusinessPartnerView // 你的系统主页或默认页面
+    component: Home // 你的系统主页或默认页面
   },
   {
     path: '/maintain-bp',
@@ -22,8 +27,21 @@ const routes = [
     name: 'MaintainBPTest',
     component: TestPageErp // 引入你的测试文件
   },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register }
+  { 
+    path: '/login',
+    name: 'Login',
+    component: Login 
+  },
+  { 
+    path: '/register', 
+    name: 'Register',
+    component: Register 
+  },
+  {
+    path: '/getMyPassBack',
+    name: 'GetMyPassBack',
+    component: getMyPassBack // 主页组件
+  }
   // 你可以在这里添加其他页面路由
 ];
 
---
+++ site/src/views/GetMyPassBack.vue
@@ -0,0 +1,7 @@
+<template>
+    <div style="text-align: center; margin-top: 50px;">
+        <h2>我们也没有办法</h2>
+        <p>您重新注册一个吧！</p>
+    </div>
+</template>
+<script lang="ts" setup></script>
