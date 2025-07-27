16c1c339243a4dec9d24670c3b59cd2f2c829760
vakesamahere <vakegamer@163.com>
2025-07-27 14:35:48 +0800

    feat: add devServer configuration for allowed hosts and network settings

--- site/vue.config.js
+++ site/vue.config.js
@@ -7,5 +7,13 @@ module.exports = {
         '@': require('path').resolve(__dirname, 'src')
       }
     }
+  },
+  devServer: {
+    allowedHosts: [
+      'ztt.aimixtech.com',
+      '192.168.10.187',
+    ],
+    host: '0.0.0.0',
+    port: 8080
   }
 }
